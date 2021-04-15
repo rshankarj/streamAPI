8ae52aab546413ccbba3405729c068b58b94cc97
rshankarj <rshankarj@gmail.com>
2021-04-15 15:16:32 +0530

    Update Sample.java
    updated Code

--- Sample.java
+++ Sample.java
@@ -1,1 +1,37 @@
+package com.app;
+
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		String[] strArray ={"practice","temp","coding","perfect"};
+		
+		String word1="practice";
+		String word2="coding";
+											
+		
+		int  distance =Integer.MAX_VALUE; ;
+		for(int x = 0; x < strArray.length; x++){
+		    if(strArray[x].equals(word1)){
+		        for(int y = x; y < strArray.length; y++){
+		            if(strArray[y].equals(word2))
+		                if(distance > (y - x))
+		                	distance = y - x;
+		        }
+		        for(int y = x; y >=0; y--){
+		            if(strArray[y].equals(word2))
+		                if(distance > (x - y))
+		                	distance = x - y;
+		        }
+		    }
+		}
+		System.out.print(distance);
+	}
+	
+	
+
+}
 
